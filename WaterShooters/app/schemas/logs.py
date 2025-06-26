8072595eb6bd528dca4ad00644fc5340897a8c91
ThUnDeR-CoDeR123 <dipnarayansen123@gmail.com>
2025-06-26 19:42:14 +0530

    imcomming quantity 3

--- WaterShooters/app/schemas/logs.py
+++ WaterShooters/app/schemas/logs.py
@@ -57,6 +57,7 @@ class ChemicalLogSchema(BaseModel):
     plant_chemical_id: Optional[int]= None
     quantity_used: Optional[float]= None
     incomming_quantity: Optional[float]= None
+    quantity_left: Optional[float]= None
     sludge_discharge: Optional[bool]  = False
     shift : Optional[int]=0 #0 for morning, 1 for evening, 2 for night
     start_date: Optional[datetime] = None
